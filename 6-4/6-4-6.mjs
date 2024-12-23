833ed2717dd1530ed3417b9ecc5e5c30a81cd370
hc421019 <nkhshc421019@g.neec.ac.jp>
2024-12-23 15:15:16 +0900

    6-4-6

---
+++ 6-4/6-4-6.mjs
@@ -0,0 +1,13 @@
+const status = "大湯沼"
+switch(status)
+{
+    case "地獄谷","大湯沼":
+        console.log("登別")
+        break
+    case "白老牛":
+        console.log("白老")
+        break
+    default:
+        console.log(status)
+        break
+}
