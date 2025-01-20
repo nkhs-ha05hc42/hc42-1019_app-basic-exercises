34739b6f0feb52a8f2dde98be7888b9aaf90b770
hc421019 <nkhshc421019@g.neec.ac.jp>
2025-01-20 15:49:48 +0900

    7-4-2

---
+++ 7-4/7-4-2.mjs
@@ -0,0 +1,12 @@
+setTimeout(() => {
+console.log("★",new Date())
+}, 2000)
+   
+setTimeout(() => {
+console.log("◆",new Date())
+    setTimeout(() => {
+    console.log("end",new Date())
+    }, 2000)
+},3000)
+
+console.log("start",new Date())
