d510ac71d314050c41dc5c4df0b1fff7773670a9
hc421019 <nkhshc421019@g.neec.ac.jp>
2025-01-20 15:45:00 +0900

    7-2-4

---
+++ 7-2/7-2-4.mjs
@@ -0,0 +1,8 @@
+import {kake} from "./7-2-4_export_kake.mjs"
+import {wari} from "./7-2-4_export_wari.mjs"
+
+const result1 = kake(3, 5)
+console.log(result1)
+
+const result2 = wari(3, 5)
+console.log(result2)
