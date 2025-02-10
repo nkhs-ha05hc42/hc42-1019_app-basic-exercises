223b7b1faccd8ee31c5eb3349279416f2b990d13
hc421019 <nkhshc421019@g.neec.ac.jp>
2025-02-10 10:42:10 +0900

    7-5-2

---
+++ 7-5/7-5-2.mjs
@@ -0,0 +1,22 @@
+const promiseSetTimeout = (timeout) =>
+    new Promise((resolve, reject) => {
+    setTimeout(() => {
+    resolve()
+    }, timeout)
+    })
+   const promiseA = () => {
+    promiseSetTimeout(2000)
+    .then(() => {
+    console.log("★",new Date())
+    })
+    promiseSetTimeout(3000)
+    .then(() => {
+    console.log("◆",new Date())
+    return promiseSetTimeout(2000)
+    })
+    .then(() => {
+    console.log("end", new Date())
+    })
+   }
+   console.log("start", new Date())
+   promiseA()
