b21baa93a4026cfbcaf6e4faee10cfa83501eefc
hc421019 <nkhshc421019@g.neec.ac.jp>
2025-02-14 16:41:24 +0900

    routers8-2

--- server/routers.mjs
+++ server/routers.mjs
@@ -4,6 +4,7 @@ import {q8_1_1Controller,
     q8_1_2Controller,
     q8_1_3Controller,
     q8_1_4Controller,
+    q8_2Controller,
     sample1Controller,
          sample2Controller} from "./api/controllers.mjs"
 
@@ -19,6 +20,10 @@ routers.get("/api/8-1", q8_1_1Controller.get811)
 routers.post("/api/8-1", q8_1_2Controller.post812)
 routers.put("/api/8-1", q8_1_3Controller.put813)
 routers.delete("/api/8-1", q8_1_4Controller.delete814)
+routers.get("/api/8-2", q8_2Controller.get821)
+routers.post("/api/8-2", q8_2Controller.post822)
+routers.put("/api/8-2", q8_2Controller.put823)
+routers.delete("/api/8-2", q8_2Controller.delete824)
 
 routers.post("/api/sample2", sample2Controller.postSample2)
 routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
