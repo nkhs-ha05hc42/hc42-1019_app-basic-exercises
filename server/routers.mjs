c10ab3f6910a1c50cc1c27d5fdef16ff0710f81f
hc421019 <nkhshc421019@g.neec.ac.jp>
2025-02-17 00:56:52 +0900

    routers

--- server/routers.mjs
+++ server/routers.mjs
@@ -6,6 +6,7 @@ import {q8_1_1Controller,
     q8_1_4Controller,
     q8_2Controller,
     q8_3Controller,
+    q9_7Controller,
     sample1Controller,
          sample2Controller} from "./api/controllers.mjs"
 
@@ -27,7 +28,11 @@ routers.put("/api/8-2", q8_2Controller.put823)
 routers.delete("/api/8-2", q8_2Controller.delete824)
 routers.get("/api/8-3", q8_3Controller.get831)
 routers.get("/api/8-3/:name", q8_3Controller.get832)
-
+routers.get("/api/9-7/:id", q9_7Controller.getOne971)
+routers.get("/api/9-7", q9_7Controller.getAll972)
+routers.post("/api/9-7", q9_7Controller.post973)
+routers.put("/api/9-7", q9_7Controller.put974)
+routers.delete("/api/9-7", q9_7Controller.delete975)
 
 
 
@@ -35,9 +40,7 @@ routers.get("/api/8-3/:name", q8_3Controller.get832)
 routers.post("/api/sample2", sample2Controller.postSample2)
 routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
 routers.get("/api/sample2", sample2Controller.getAllSample2)
-
 routers.put("/api/sample2/:id", sample2Controller.putSample2)
-
 routers.delete("/api/sample2/:id", sample2Controller.deleteSample2)
 
 // client配下は、ブラウザサイドで実行されるファイル（HTML/JS/CSS/画像など）を配置する
